5b72fefcc0949d038b720b9f44d0b303d295f86a
manivenkat-lab <manivenkat0666@gmail.com>
2026-03-08 19:57:02 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,18 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || ""
+  apiKey: "AIzaSyCdI-PdsiYOLkFrbgK8gM3K3s12rvONCj4",
+  authDomain: "m-homes1.firebaseapp.com",
+  projectId: "m-homes1",
+  storageBucket: "m-homes1.firebasestorage.app",
+  messagingSenderId: "759137259473",
+  appId: "1:759137259473:web:1275fb062bad4d77b21af0",
+  measurementId: "G-D9FW9833H6"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+
 
 
 
